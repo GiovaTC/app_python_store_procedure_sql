a2d67e928a6cb3082c7a8ed7af9149f4bd5f56ff
gtapiero <giovanny.tapiero@pi.edu.co>
2025-09-02 18:32:22 -0500

    cuarto commit

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def upsert_usuario(nombre: str, edad: int, email: str, id_existente: int | None 
     Devuelve: (id_resultante, accion, fila_dict)
     """
     with pyodbc.connect(CONN_STR, autocommit=True) as conn:
-        with conn.cursor()_as cur:
+        with conn.cursor() as cur:
             tsql = """
                 DECLARE @outId INT = ?; -- recibimos el posible Id inicial
                 EXEC dbo.usp_Usuarios_Upsert
@@ -53,13 +53,20 @@ def upsert_usuario(nombre: str, edad: int, email: str, id_existente: int | None 
             fila["Id"] = final_id
 
             return final_id, accion, fila
-if__name__ == "__main__":
+if __name__ == "__main__":
 # ---- INSERT ----
-nuevo_id, accion_ins, fila_ins = upsert_usuario(
+    nuevo_id, accion_ins, fila_ins = upsert_usuario(
     nombre="Carlos Pérez",
     edad=28,
     email="carlos.perez@exam.com"
 )
 print(f"[{accion_ins}] Nuevo Id: {nuevo_id} -> {fila_ins}")
 # ---- UPDATE ----
+id_actualizado, accion_upd, fila_upd = update_usuario(
+    nombre ="Carlos A. Pérez",
+    edad = 29,
+    email = "carlos.perez@exam.com",
+    id_existente=nuevo_id
+)
+print(f"[{accion_upd}] Id: {id_actualizado} -> {fila_upd}")
 
